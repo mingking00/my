be3a19045b3b9e39b70dfcbceee1d4c25a65e911
mingking00 <480432839@qq.com>
2026-03-16 13:39:30 +0800

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'My Blog',
+  description: 'My personal blog'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="zh">
+      <body>{children}</body>
+    </html>
+  )
+}
